cd056e4087463bcef87b9c8b0cad9616605641fe
Yuugen <yuugenssb@proton.me>
2025-06-21 04:53:46 +0200

    WIP(launcher): rewrite using stdenv.mkDerivation
    Signed-off-by: Yuugen <yuugenssb@proton.me>

--- pkgs/slippi-launcher/default.nix
+++ pkgs/slippi-launcher/default.nix
@@ -1,50 +1,56 @@
 {
   electron,
   fetchFromGitHub,
+  fetchYarnDeps,
   lib,
   makeDesktopItem,
   makeWrapper,
-  mkYarnPackage,
-  slippi-netplay,
+  nodejs,
   replaceVars,
+  slippi-netplay,
+  stdenv,
+  yarnBuildHook,
+  yarnConfigHook,
 }:
 
-let
+stdenv.mkDerivation (finalAttrs: {
   pname = "slippi-launcher";
   version = "2.11.10";
   src = fetchFromGitHub {
     owner = "project-slippi";
     repo = "slippi-launcher";
-    tag = "v${version}";
+    tag = "v${finalAttrs.version}";
     hash = "sha256-JrM2nm5iEAoyrGeqF1iP+kKjdiC/3mfCihzawg3Xv9s=";
   };
-in
-mkYarnPackage {
-  inherit pname version src;
-  packageJSON = "${src}/package.json";
-  yarnLock = "${src}/yarn.lock";
+  offlineCache = fetchYarnDeps {
+    yarnLock = "${finalAttrs.src}/yarn.lock";
+    hash = "sha256-zrN8ZY6ZO/L4gDufWY1Ts00mauAaQxyQTbaqjKBt0kI=";
+  };
+
   nativeBuildInputs = [
     makeWrapper
+    nodejs
+    yarnBuildHook
+    yarnConfigHook
   ];
 
-  ELECTRON_SKIP_BINARY_DOWNLOAD = "1";
+  env.ELECTRON_SKIP_BINARY_DOWNLOAD = "1";
   patches = [
     (replaceVars ./0001-replace-findDolphinExecutable-by-NIX_DOLPHIN_PATH-va.patch {
       NIX_DOLPHIN_PATH = "${lib.getExe slippi-netplay}";
     })
   ];
 
-  postBuild = ''
-    export HOME=$(mktemp -d)
-    yarn --offline run package
-      --dir \
-      -c.electronDist=${electron}/lib/electron \
-      -c.electronVersion=${electron.version}
-  '';
+  yarnBuildScript = "run package";
+  yarnBuildFlags = [
+    "--dir"
+    "-c.electronDist=${electron.dist}"
+    "-c.electronVersion=${electron.version}"
+  ];
   installPhase = ''
     runHook preInstall
 
-    makeWrapper "${lib.getExe electron}" "$out/bin/${pname}" \
+    makeWrapper "${lib.getExe electron}" "$out/bin/${finalAttrs.pname}" \
       --add-flags "\''${NIXOS_OZONE_WL:+\''${WAYLAND_DISPLAY:+--ozone-platform-hint=auto --enable-features=WaylandWindowDecorations}}" \
       --inherit-argv0
 
@@ -53,7 +59,7 @@ mkYarnPackage {
 
   desktopItems = [
     (makeDesktopItem {
-      name = pname;
+      name = finalAttrs.pname;
       desktopName = "Slippi Launcher";
       exec = "slippi-launcher %U";
       icon = "slippi-launcher";
@@ -73,9 +79,10 @@ mkYarnPackage {
   ];
 
   meta = {
+    mainProgram = finalAttrs.pname;
     homepage = "https://slippi.gg";
     description = "The way to play Slippi Online and watch replays";
-    platforms = lib.platforms.linux;
+    platforms = lib.platforms.unix;
     license = lib.licenses.gpl3Only;
   };
-}
+})
